f43dff4f5c1ad5534157c8aa920d13c201e1f600
Pooja Raut <rautpooja095@gmail.com>
2024-07-25 02:41:43 -0500

    Create Addition & substraction.c

---
+++ Addition & substraction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int a,b,sub,sum;
+	printf(" \n Enter int a:");
+	printf("\n Enter int b:");
+	scanf("%d %d",&a,&b);
+	sub = a-b;
+  sum= a+b;
+	printf("\n The sub is:%d",sub);
+  printf("\n The sum is %d",sum);
+	return 0;
+	}
