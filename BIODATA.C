50ad9032b1471e22d8114d4b6c648adc041e654c
Pooja Raut <rautpooja095@gmail.com>
2024-07-25 02:25:46 -0500

    Create BIODATA.C

---
+++ BIODATA.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	printf("\n...........Biodata...........");
+	printf("\n Name: Pooja Pramod Raut");
+	printf("\n Colleage Name: ADYPU");
+	printf("\n Mob.No: 8308712406");
+	printf("\n Address: Pune");
+	return 0;
+}
